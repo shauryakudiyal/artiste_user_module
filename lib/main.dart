f95e49cb5fcabb01096a79dfdbc3929c411c7fe5
shauryakudiyal <59408202+shauryakudiyal@users.noreply.github.com>
2020-06-11 22:55:25 +0530

    1.0.0

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:artiste/signinform/Loginpage.dart';
+import 'package:flutter/material.dart';
+
+import 'firbase.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.grey,
+      ),
+      home: MyApp1(),
+    );
+  }
+}
+
+
